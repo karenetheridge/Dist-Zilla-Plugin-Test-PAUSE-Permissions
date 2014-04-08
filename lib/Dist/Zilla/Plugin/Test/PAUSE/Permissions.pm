32f110b4806f93ed280d445e56f7675b98017050
Karen Etheridge <ether@cpan.org>
2014-04-07 19:30:54 -0700

    initial code, docs, tests

--- lib/Dist/Zilla/Plugin/Test/PAUSE/Permissions.pm
+++ lib/Dist/Zilla/Plugin/Test/PAUSE/Permissions.pm
@@ -1,31 +1,90 @@
 use strict;
 use warnings;
 package Dist::Zilla::Plugin::Test::PAUSE::Permissions;
-# ABSTRACT: ...
+# ABSTRACT: Generate a test to verify PAUSE permissions
 # vim: set ts=8 sw=4 tw=78 et :
 
+use Moose;
+with (
+    'Dist::Zilla::Role::FileGatherer',
+    'Dist::Zilla::Role::TextTemplate',
+    'Dist::Zilla::Role::PrereqSource',
+);
 
+use Path::Tiny;
+use namespace::autoclean;
 
-1;
+sub filename { path(qw(xt release pause-permissions.t))->stringify }
+
+has username => (
+    is => 'ro', isa => 'Str|Undef',
+    lazy => 1,
+    default => sub {
+        my $self = shift;
+        my $stash = $self->zilla->stash_named('%PAUSE');
+        return if not $stash;
+
+        my $username = $stash->username;
+        $self->log_debug([ 'using PAUSE id "%s" from Dist::Zilla config', $username ]) if $username;
+        $username;
+    },
+);
+
+sub register_prereqs
+{
+    my $self = shift;
+
+    $self->zilla->register_prereqs(
+        {
+            type  => 'requires',
+            phase => 'develop',
+        },
+        'Test::PAUSE::Permissions' => '0',
+    );
+}
+
+sub gather_files
+{
+    my $self = shift;
+
+    require Dist::Zilla::File::InMemory;
+    $self->add_file(Dist::Zilla::File::InMemory->new(
+        name => $self->filename,
+        content => $self->fill_in_string(
+            <<'TEST',
+use strict;
+use warnings;
+
+# this test was generated with {{ ref($plugin) . ' ' . ($plugin->VERSION || '<self>') }}
+
+use Test::PAUSE::Permissions;
+all_permissions_ok({{ $username ? qq{'$username'} : '' }});
+TEST
+            {
+                dist => \($self->zilla),
+                plugin => \$self,
+                username => \($self->username),
+            },
+        ),
+    ));
+}
+
+__PACKAGE__->meta->make_immutable;
 __END__
 
 =pod
 
 =head1 SYNOPSIS
 
-    use Dist::Zilla::Plugin::Test::PAUSE::Permissions;
+In your F<dist.ini>:
 
-    ...
+    [Test::PAUSE::Permissions]
 
 =head1 DESCRIPTION
 
-...
-
-=head1 FUNCTIONS/METHODS
-
-=head2 C<foo>
-
-...
+This is a L<Dist::Zilla> plugin that runs at the
+L<gather files|Dist::Zilla::Role::FileGatherer> stage, providing a
+L<Test::PAUSE::Permisisons> test, named F<xt/release/pause-permissions.t>).
 
 =head1 SUPPORT
 
@@ -35,15 +94,11 @@ Bugs may be submitted through L<the RT bug tracker|https://rt.cpan.org/Public/Di
 (or L<bug-Dist-Zilla-Plugin-Test-PAUSE-Permissions@rt.cpan.org|mailto:bug-Dist-Zilla-Plugin-Test-PAUSE-Permissions@rt.cpan.org>).
 I am also usually active on irc, as 'ether' at C<irc.perl.org>.
 
-=head1 ACKNOWLEDGEMENTS
-
-...
-
 =head1 SEE ALSO
 
 =begin :list
 
-* L<foo>
+* L<Test::PAUSE::Permisisons>
 
 =end :list
 
--- t/01-basic.t
+++ t/01-basic.t
@@ -3,8 +3,69 @@ use warnings FATAL => 'all';
 
 use Test::More;
 use if $ENV{AUTHOR_TESTING}, 'Test::Warnings';
-use Dist::Zilla::Plugin::Test::PAUSE::Permissions;
+use Test::DZil;
+use Path::Tiny;
+use File::pushd 'pushd';
 
-fail('this test is TODO!');
+{
+    my $tzil = Builder->from_config(
+        { dist_root => 't/does-not-exist' },
+        {
+            add_files => {
+                path(qw(source dist.ini)) => simple_ini(
+                    [ GatherDir => ],
+                    [ 'Test::PAUSE::Permissions' ],
+                    [ '%PAUSE' => { username => 'username', password => 'password' } ],
+                ),
+                path(qw(source lib Foo.pm)) => "package Foo;\n1;\n",
+            },
+        },
+    );
+    $tzil->build;
+
+    my $build_dir = path($tzil->tempdir)->child('build');
+    my $file = $build_dir->child(qw(xt release pause-permissions.t));
+    ok(-e $file, 'test created');
+
+    my $content = $file->slurp_utf8;
+    unlike($content, qr/[^\S\n]\n/m, 'no trailing whitespace in generated test');
+
+    like($content, qr/^all_permissions_ok\('username'\);$/m, 'username extracted from stash and passed to test');
+
+    subtest 'run the generated test' => sub
+    {
+        my $wd = pushd $build_dir;
+
+        # ensure we don't call out to the network when running the test
+        local $ENV{RELEASE_TESTING};
+        do $file;
+        warn $@ if $@;
+    };
+}
+
+{
+    my $tzil = Builder->from_config(
+        { dist_root => 't/does-not-exist' },
+        {
+            add_files => {
+                path(qw(source dist.ini)) => simple_ini(
+                    [ GatherDir => ],
+                    [ 'Test::PAUSE::Permissions' ],
+                ),
+                path(qw(source lib Foo.pm)) => "package Foo;\n1;\n",
+            },
+        },
+    );
+    $tzil->build;
+
+    my $build_dir = path($tzil->tempdir)->child('build');
+    my $file = $build_dir->child(qw(xt release pause-permissions.t));
+    ok(-e $file, 'test created');
+
+    my $content = $file->slurp_utf8;
+    unlike($content, qr/[^\S\n]\n/m, 'no trailing whitespace in generated test');
+
+    like($content, qr/^all_permissions_ok\(\);$/m, 'no username passed to test');
+}
 
 done_testing;
